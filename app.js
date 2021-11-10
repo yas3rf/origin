7c71a4a32ffc3980f07fe731004943534bb88688
yas3rf <faqiriyaser@gmail.com>
2021-11-10 16:37:02 +0530

    Hashing function

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 //jshint esversion:6
-require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -18,8 +17,6 @@ const userSchema = new mongoose.Schema({
   password: "String"
 });
 
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -40,7 +37,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -54,7 +51,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const userName = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email: userName},function(err, results){
     if(err){
       console.log(err);
