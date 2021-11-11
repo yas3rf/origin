2eeb82844fbef91e1e4001fa03a2c03904797fc2
yas3rf <faqiriyaser@gmail.com>
2021-11-11 16:46:57 +0530

    Use bcrypt hashing

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -35,9 +36,12 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
+
+bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+
   const newUser = new User({
     email: req.body.username,
-    password: md5(req.body.password)
+    password: hash
   });
 
   newUser.save(function(err){
@@ -49,17 +53,24 @@ app.post("/register", function(req, res){
   });
 });
 
+});
+
 app.post("/login", function(req, res){
+
   const userName = req.body.username;
-  const password = md5(req.body.password);
-  User.findOne({email: userName},function(err, results){
+  const password = req.body.password;
+  User.findOne({email: userName},function(err, found){
     if(err){
       console.log(err);
     } else{
-      if(results){
-        if(results.password === password){
-          res.render("secrets");
-        }
+      if(found){
+        bcrypt.compare(password, found.password, function(err, result){
+          if(result === true){
+            res.render("secrets");
+          }else {
+            console.log(err);
+          }
+        });
       }
     }
   });
